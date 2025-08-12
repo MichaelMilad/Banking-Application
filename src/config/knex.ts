6bc360fc8bae794985e63ea3ee96f78d433aedab
Michael Milad <michaelmilad001@gmail.com>
2025-08-13 00:26:55 +0300

    initial file structure and first knex migrate

---
+++ src/config/knex.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import type { Knex } from 'knex';
+
+export const knexConfig: Knex.Config = {
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '12345',
+    database: process.env.DB_NAME || 'bank_app',
+    port: Number(process.env.DB_PORT) || 3306,
+  },
+  migrations: {
+    directory: './src/database/migrations'
+  },
+  seeds: {
+    directory: './src/database/seeds'
+  }
+};
